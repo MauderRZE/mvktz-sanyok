328d16677d7c82827a3813bcb838cb2249d0e495
cordador <cordador@mail.com>
2026-07-08 16:03:09 +0300

    refactor: clean up temporary test files and debug scripts

--- render-livewire.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-$html = Illuminate\Support\Facades\Blade::render('<x-ui.modal title="Test" maxWidth="2xl">hello</x-ui.modal>');
-file_put_contents('/tmp/modal-test.html', $html);
-echo "Done\n";
