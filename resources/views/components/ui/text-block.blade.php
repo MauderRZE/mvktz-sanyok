f6a2aef2f1ded7806e6c40b97ba67600dec1e882
cordador <cordador@mail.com>
2026-07-09 10:57:02 +0300

    fix: decode HTML entities in text-block component title and subtitle outputs

--- resources/views/components/ui/text-block.blade.php
+++ resources/views/components/ui/text-block.blade.php
@@ -1,8 +1,8 @@
 @props(['title', 'subtitle' => null, 'titleClass' => 'text-sm text-white font-medium', 'subtitleClass' => 'text-xs text-gray-500'])
 
 <div {{ $attributes }}>
-    <p class="{{ $titleClass }}">{{ $title }}</p>
+    <p class="{{ $titleClass }}">{{ html_entity_decode($title, ENT_QUOTES, 'UTF-8') }}</p>
     @if($subtitle)
-        <p class="{{ $subtitleClass }} mt-0.5">{{ $subtitle }}</p>
+        <p class="{{ $subtitleClass }} mt-0.5">{{ html_entity_decode($subtitle, ENT_QUOTES, 'UTF-8') }}</p>
     @endif
 </div>
